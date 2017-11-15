f7e3458338c6c331b5eda314aae89f79f234a1dd
Daphnie Churchill <otterchurchill@gmail.com>
2017-11-15 14:05:10 -0800

    Accuracy&Loadings

--- LDAClassifier.py
+++ LDAClassifier.py
@@ -1,4 +1,4 @@
-# To run python3.5 TypeAData.txt TypeACellCalasification.txt TypeBData TypeBClassif .... > StepInfo
+# To run python3.5 TypeAData.txt TypeACellCalasification.txt TypeBData TypeBClassif (.#)precentsplit outname> StepInfo
 
 import sys
 import pandas as pd
@@ -7,35 +7,52 @@ import matplotlib.patches as mpatches
 import numpy as np
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from scipy import stats
+from sklearn.utils import shuffle
 
-def isSortedFiles(dataUIDs, typeUIDs, name):
-    if dataUIDs.equals(typeUIDs):
+#********************************************************************************
+def isSortedFiles(listOneUIDs, listTwoUIDs, name):
+    if listOneUIDs[1:].equals(listTwoUIDs[1:]):
         print(name, "is ordered identically")
 
     else:
+        for listOneI, listTwoI in zip(listOneUIDs,listTwoUIDs):
+            print("listOneUIDs:", listOneI)
+            print("listTwoUIDs:", listTwoI)
+            if listOneI != listTwoI:
+                print("something stinks")
+
+
         print("ERROR:", name, "is not ordered correctly for program to run")
-        print("please sort -k1 on both, ending script now")
+        print("please sort both, ending script now")
         sys.exit()
 
-def makePD(name):
+#********************************************************************************
+def makePD(name, needColumns=False):
     infile = open(name, 'r')
     
     data = pd.read_csv(infile, sep = '\t')
-    data.columns = ["C" + str(i) for i in range(1, len(data.columns)+1)]
+    columns = data.columns
+    columns = columns[1:]
 
     infile.close()
+    
+    if needColumns:
+        return data, columns
+    else:
+        return data
 
-    return data
-
+#********************************************************************************
 def isPD(objectToCheck, name):
     if type(objectToCheck) != "pandas.core.frame.DataFrame":
         print("False",name, "is a", type(objectToCheck))
     
+#********************************************************************************
 def split(precentNeeded,typeA,typeB,typeACell, typeBCell):
     typeATrainNum, typeATestNum = getTrainTest(precentNeeded, len(typeA.index))
     typeBTrainNum, typeBTestNum = getTrainTest(precentNeeded, len(typeB.index))
     
-    print("train:", typeATrainNum, "test:", typeATestNum)  
+    print("trainA:", typeATrainNum, "testA:", typeATestNum)  
+    print("trainB:", typeBTrainNum, "testB:", typeBTestNum)  
 
     typeATrain = typeA.loc[ : typeATrainNum - 1, :]
     typeBTrain = typeB.loc[ : typeBTrainNum - 1, : ]
@@ -55,6 +72,7 @@ def split(precentNeeded,typeA,typeB,typeACell, typeBCell):
     print("cellTest:\n", cellTrain)
     return train, test, cellTrain, cellTest
 
+#********************************************************************************
 def getTrainTest(precentNeeded, lenght):
     print("lenght",lenght) 
     trainNum = round(precentNeeded * lenght)
@@ -62,11 +80,19 @@ def getTrainTest(precentNeeded, lenght):
 
     return trainNum, testNum
 
-def predict(train, test , trainClassifs):
+#********************************************************************************
+def predict(train, test , trainClassifs, varibles):
     lda = LDA(n_components=2) 
     lda = lda.fit(train, trainClassifs) 
     X_lda = lda.transform(train) 
-
+    
+    
+    print('{:*^70}'.format('*'))
+    print('{:^70}'.format('LinearDiscriminantAnalysis'))
+    print('{:*^70}'.format('*'))
+    
+    scalings(lda, train, varibles, True)
+    
     YTest = lda.transform(test) 
     YClasifs = lda.predict(test) 
     YClasifsProbability = lda.predict_proba(test) 
@@ -74,7 +100,54 @@ def predict(train, test , trainClassifs):
     print("lda.trans(test):\n", YTest)
     print("lda.predict(test):\n", YClasifs)
     print("predict_prob(test)\n", YClasifsProbability)
+    return YClasifs
+
+#********************************************************************************
+def randomize(train, test, trainClassifs, testClassifs, state):
     
+    concat = pd.concat([train, trainClassifs], axis=1)
+    print("randomize:\n", concat)
+    concatRand = concat.sample(frac=1, random_state=state)
+    concatRand.reset_index(drop =True)
+    return concatRand
+
+
+#********************************************************************************
+def getAccuracy(testClassifs, prediction):
+    print (prediction,'\n', sep='')
+    print(testClassifs.tolist(),'\n', sep='')
+    
+    testClassifs = testClassifs.tolist()
+    
+    correctPredictionsSummed = 0
+
+    for correct,predict in zip(testClassifs,prediction):
+        if correct == predict:
+            correctPredictionsSummed += 1
+    
+    correctPredictions = (correctPredictionsSummed / len(testClassifs))
+    print("#OfCorrectPredictions:", correctPredictionsSummed, "#ofPredictions:",len(testClassifs))
+    print( "Accuracy:", correctPredictions)
+        
+#********************************************************************************
+def scalings(lda, X, varibles, out=False):
+#source:http://python-for-multivariate-analysis.readthedocs.io/a_little_book_of_python_for_multivariate_analysis.html#loadings-for-the-discriminant-functions
+    ret = pd.DataFrame(lda.scalings_, index=X.columns, columns=["LD"+str(i+1) for i in range(lda.scalings_.shape[1])])
+    varibles = pd.DataFrame( varibles)
+    if out:
+        pd.set_option('display.max_rows', len(ret))
+        pd.options.display.float_format = '{:,.12f}'.format
+        print("Coefficients of linear discriminants:")
+        #concat = pd.concat([varibles, ret], axis=1)
+        #print("concat", concat)
+        #print("ret\n",ret, sep='')
+        retSorted = ret.reindex(ret.LD1.abs().sort_values(ascending=False).index)
+        
+        print("SortedLDAVars\n", retSorted, sep='')
+
+    return ret
+
+#********************************************************************************
 
 def main():
     
@@ -86,37 +159,39 @@ def main():
     
     percent = float(percent)
 
-    typeAData = makePD(typeAfileName)
-    typeBData = makePD(typeBfileName)
+    typeAData, AColumnHead = makePD(typeAfileName, True)
+    typeBData,BColumnHead = makePD(typeBfileName, True)
     
     typeACellData = makePD(typeACellFile)
     typeBCellData = makePD(typeBCellFile)
     
-    print("Step1: Split Ratio is", percent)
+    print("Split Ratio is", percent)
     
     print(typeAData)
 
-    print("Cell\n", typeACellData)
-    print("Cell\n", typeBCellData)
+    print("CellA\n", typeACellData)
+    print("CellB\n", typeBCellData)
 
-    xtypeAUID = typeAData.C1
-    yTypeACellUID = typeACellData.C1
+    xtypeAUID = typeAData.iloc[:, 0]
+    yTypeACellUID = typeACellData.iloc[:, 0]
     
     isSortedFiles(xtypeAUID, yTypeACellUID, "TypeA")
-    
-    xtypeBUID = typeBData.C1
-    yTypeBCellUID = typeBCellData.C1
+    xtypeBUID = typeBData.iloc[:, 0]
+    yTypeBCellUID = typeBCellData.iloc[:, 0]
     
     isSortedFiles(xtypeBUID, yTypeBCellUID, "TypeB")
+    isSortedFiles(AColumnHead, BColumnHead, "columnHead")
 
-    xtypeA = typeAData.loc[:, "C2":]
-    ytypeA = typeACellData.C2
+    xtypeA = typeAData.iloc[:, 1:]
+    ytypeA = typeACellData.iloc[:, 1]
 
-    xtypeB = typeBData.loc[:, "C2":]
-    ytypeB = typeBCellData.C2
+    xtypeB = typeBData.iloc[:, 1:]
+    ytypeB = typeBCellData.iloc[:, 1]
     
     train, test, trainClassifs, testClassifs, =split(percent, xtypeA, xtypeB, ytypeA, ytypeB)
-    predict(train, test, trainClassifs)
+    prediction = predict(train, test, trainClassifs, AColumnHead)
+    getAccuracy(testClassifs, prediction)
+
 
 if __name__ == "__main__":
     main()
